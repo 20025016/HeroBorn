781c6a5933b98b5e93155171f519036fab862cd0
Joshua Ryan-Sprowson <20025016@tafe.wa.edu.au>
2023-03-17 08:25:05 +0800

    New c# topics
    Common built-in types, string interpolation, type conversions, inferred declarations, naming variables

--- Assets/Scripts/LearningCurve.cs
+++ Assets/Scripts/LearningCurve.cs
@@ -6,10 +6,30 @@ public class LearningCurve : MonoBehaviour
 {
     public int CurrentAge = 30;
     public int AddedAge = 1;
+
+    public float Pi = 3.14f;
+    public string FirstName = "Harrison";
+    public bool IsAuthor = true;
+
+    public int MaxPlayerHealth = 100;
+
     // Start is called before the first frame update
     void Start()
     {
         ComputeAge();
+        Debug.Log($"A string can have variables like {FirstName} inserted directly!");
+
+        int myInteger = 3;
+        float myFloat = myInteger;
+
+        Debug.Log(myInteger);
+        Debug.Log(myFloat);
+
+        int explicitConversion = (int)3.14;
+        Debug.Log(explicitConversion);
+
+        var currentAge = 32;
+        Debug.Log(currentAge);
     }
 
     /// <summary>
