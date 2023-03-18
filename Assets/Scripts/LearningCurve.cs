38315df05246936fc3dc80dc597cbe3288cb87bd
Joshua Ryan-Sprowson <20025016@tafe.wa.edu.au>
2023-03-18 18:43:23 +0800

    Finish naming conventions, methods as logic detours sub-section of defining methods

--- Assets/Scripts/LearningCurve.cs
+++ Assets/Scripts/LearningCurve.cs
@@ -7,7 +7,9 @@ public class LearningCurve : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Debug.Log("\"Choose\" a character.");
         GenerateCharacter();
+        Debug.Log("A fine choice.");
     }
 
     // Update is called once per frame
