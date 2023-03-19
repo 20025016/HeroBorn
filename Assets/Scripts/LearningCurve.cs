01df448ed027da6000fa1e3162640b94898fafb8
Joshua Ryan-Sprowson <20025016@tafe.wa.edu.au>
2023-03-19 20:23:53 +0800

    Finish accessing and modifying subsection of lists section

--- Assets/Scripts/LearningCurve.cs
+++ Assets/Scripts/LearningCurve.cs
@@ -13,12 +13,25 @@ public class LearningCurve : MonoBehaviour
             "Sterling the Knight"
         };
 
+        questPartyMembers.Add("Craven the Necromancer");
+
+        questPartyMembers.Insert(1, "Tanis the Thief");
+
+        questPartyMembers.RemoveAt(0);
+        //questPartyMembers.Remove("Grim the Barbarian");
+
         Debug.LogFormat("Party Members: {0}", questPartyMembers.Count);
 
         List<int> playerScores = new()
         {
             30, 45, 50
         };
+
+        playerScores.Add(65);
+
+        playerScores.Insert(1, 55);
+
+        playerScores.Remove(45);
 
         Debug.LogFormat("Number of scores: {0}", playerScores.Count);
     }
