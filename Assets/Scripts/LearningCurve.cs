1631909f295f0d404e636641d7e5b1d19c621e41
Joshua Ryan-Sprowson <20025016@tafe.wa.edu.au>
2023-03-19 12:12:19 +0800

    Singular variable names

--- Assets/Scripts/LearningCurve.cs
+++ Assets/Scripts/LearningCurve.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public class LearningCurve : MonoBehaviour
 {
     // Empty array with default values
-    private int[] Teams1 = new int[3];
+    private int[] Team1 = new int[3];
     // Longhand initialiser
-    private int[] Teams2 = new int[] { 713, 549, 984 };
+    private int[] Team2 = new int[] { 713, 549, 984 };
     // Shorthand initialiser
-    private int[] Teams3 = { 713, 549, 984 };
+    private int[] Team3 = { 713, 549, 984 };
 }
