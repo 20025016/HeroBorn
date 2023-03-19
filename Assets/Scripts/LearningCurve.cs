9d4d45c54b1545c7bedb2843cfadd4bdb0dca171
Joshua Ryan-Sprowson <20025016@tafe.wa.edu.au>
2023-03-19 12:19:42 +0800

    Finish indexing and subscripts subsection of arrays section
    Forgot to include modifying an element in an array

--- Assets/Scripts/LearningCurve.cs
+++ Assets/Scripts/LearningCurve.cs
@@ -10,4 +10,19 @@ public class LearningCurve : MonoBehaviour
     private int[] Team2 = new int[] { 713, 549, 984 };
     // Shorthand initialiser
     private int[] Team3 = { 713, 549, 984 };
+
+    private void Start()
+    {
+        /*
+         * Arrays zero index based
+         * Team2[1] results in 549
+         */
+        Debug.Log(Team2[1]);
+        /*
+         * Modifying an element 
+         * Team2[1] results in 550
+         */
+        Team2[1] = 550;
+        Debug.Log(Team2[1]);
+    }
 }
