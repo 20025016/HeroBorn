08f2f866334c79be740680f541056f967a8e88f7
Joshua Ryan-Sprowson <20025016@tafe.wa.edu.au>
2023-03-19 19:56:58 +0800

    Finish introduction subsection of lists section

--- Assets/Scripts/LearningCurve.cs
+++ Assets/Scripts/LearningCurve.cs
@@ -4,30 +4,22 @@ using UnityEngine;
 
 public class LearningCurve : MonoBehaviour
 {
-    // Empty array with default values
-    private int[] Team1 = new int[3];
-    // Longhand initialiser
-    private int[] Team2 = new int[] { 713, 549, 984 };
-    // Shorthand initialiser
-    private int[] Team3 = { 713, 549, 984 };
-
     private void Start()
     {
-        /*
-         * Arrays zero index based
-         * Team2[1] results in 549
-         */
-        Debug.Log(Team2[1]);
-        /*
-         * Modifying an element 
-         * Team2[1] results in 550
-         */
-        Team2[1] = 550;
-        Debug.Log(Team2[1]);
+        List<string> questPartyMembers = new List<string>()
+        {
+            "Grim the Barbarian",
+            "Merlin the Wise",
+            "Sterling the Knight"
+        };
 
-        // Range exceptions can be avoided by getting the length of the array
-        Debug.LogFormat("Team1 size: {0}", Team1.Length);
-        Debug.LogFormat("Team2 size: {0}", Team2.Length);
-        Debug.LogFormat("Team3 size: {0}", Team3.Length);
+        Debug.LogFormat("Party Members: {0}", questPartyMembers.Count);
+
+        List<int> playerScores = new()
+        {
+            30, 45, 50
+        };
+
+        Debug.LogFormat("Number of scores: {0}", playerScores.Count);
     }
 }
