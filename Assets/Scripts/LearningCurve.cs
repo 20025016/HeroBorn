a2518efc247851ffff0f55d8cfb76618ab7d9622
Joshua Ryan-Sprowson <20025016@tafe.wa.edu.au>
2023-03-19 20:43:48 +0800

    Finish introduction subsection of dictionaries section

--- Assets/Scripts/LearningCurve.cs
+++ Assets/Scripts/LearningCurve.cs
@@ -6,33 +6,13 @@ public class LearningCurve : MonoBehaviour
 {
     private void Start()
     {
-        List<string> questPartyMembers = new List<string>()
+        Dictionary<string, int> itemInventory = new Dictionary<string, int>()
         {
-            "Grim the Barbarian",
-            "Merlin the Wise",
-            "Sterling the Knight"
+            { "Potion", 5 },
+            { "Antidote", 7 },
+            { "Aspirin", 1 }
         };
 
-        questPartyMembers.Add("Craven the Necromancer");
-
-        questPartyMembers.Insert(1, "Tanis the Thief");
-
-        questPartyMembers.RemoveAt(0);
-        //questPartyMembers.Remove("Grim the Barbarian");
-
-        Debug.LogFormat("Party Members: {0}", questPartyMembers.Count);
-
-        List<int> playerScores = new()
-        {
-            30, 45, 50
-        };
-
-        playerScores.Add(65);
-
-        playerScores.Insert(1, 55);
-
-        playerScores.Remove(45);
-
-        Debug.LogFormat("Number of scores: {0}", playerScores.Count);
+        Debug.LogFormat("Items: {0}", itemInventory.Count);
     }
 }
