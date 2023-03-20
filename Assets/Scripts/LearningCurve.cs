230363ff17b1b2eb5189c55c007adf9b43a21b44
Joshua Ryan-Sprowson <20025016@tafe.wa.edu.au>
2023-03-20 21:49:37 +0800

    Finish introduction into foreach loops of iteration statements section

--- Assets/Scripts/LearningCurve.cs
+++ Assets/Scripts/LearningCurve.cs
@@ -6,11 +6,6 @@ public class LearningCurve : MonoBehaviour
 {
     private void Start()
     {
-        FindPartyMember();
-    }
-
-    public void FindPartyMember()
-    {
         List<string> questPartyMembers = new List<string>()
         {
             "Grim the Barbarian",
@@ -18,16 +13,9 @@ public class LearningCurve : MonoBehaviour
             "Sterling the Knight"
         };
 
-        int questPartySize = questPartyMembers.Count;
-
-        Debug.LogFormat("Quest Party Size: {0}", questPartySize);
-        for (int i = 0; i < questPartySize; i++)
+        foreach (string partyMember in questPartyMembers)
         {
-            Debug.LogFormat("Index: {0} - {1}", i, questPartyMembers[i]);
-            if (questPartyMembers[i] == "Merlin the Wise")
-            {
-                Debug.Log("Glad you're here Merlin!");
-            }
+            Debug.LogFormat("{0} - Here!", partyMember);
         }
     }
 }
