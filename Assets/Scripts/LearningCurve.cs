fd3dae3ee6680b3a22303d5365d015826177993f
Joshua Ryan-Sprowson <20025016@tafe.wa.edu.au>
2023-03-20 22:18:48 +0800

    Finish while loops of iteration statements section

--- Assets/Scripts/LearningCurve.cs
+++ Assets/Scripts/LearningCurve.cs
@@ -4,25 +4,20 @@ using UnityEngine;
 
 public class LearningCurve : MonoBehaviour
 {
+    public int PlayerLives = 3;
+
     private void Start()
     {
-        int playerGold = 8;
+        HealthStatus();
+    }
 
-        Dictionary<string, int> itemInventory = new Dictionary<string, int>()
+    public void HealthStatus()
+    {
+        while (PlayerLives > 0)
         {
-            { "Potion", 5 },
-            { "Antidote", 7 },
-            { "Aspirin", 1 }
-        };
-
-        foreach(KeyValuePair<string, int> kvp in itemInventory)
-        {
-            Debug.LogFormat("Item: {0} - {1}g", kvp.Key, kvp.Value);
-
-            if (playerGold >= kvp.Value)
-            {
-                Debug.LogFormat("This {0} item is affordable", kvp.Key);
-            }
+            Debug.Log("Still alive!");
+            PlayerLives--;
         }
+        Debug.Log("Player KO'd...");
     }
 }
