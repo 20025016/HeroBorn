876b29cdd7baadf3a577eeb30c27ce6562157d8d
Joshua Ryan-Sprowson <20025016@tafe.wa.edu.au>
2023-03-20 21:59:16 +0800

    Finish looping through key-value pairs of iteration statements section

--- Assets/Scripts/LearningCurve.cs
+++ Assets/Scripts/LearningCurve.cs
@@ -6,16 +6,16 @@ public class LearningCurve : MonoBehaviour
 {
     private void Start()
     {
-        List<string> questPartyMembers = new List<string>()
+        Dictionary<string, int> itemInventory = new Dictionary<string, int>()
         {
-            "Grim the Barbarian",
-            "Merlin the Wise",
-            "Sterling the Knight"
+            { "Potion", 5 },
+            { "Antidote", 7 },
+            { "Aspirin", 1 }
         };
 
-        foreach (string partyMember in questPartyMembers)
+        foreach(KeyValuePair<string, int> kvp in itemInventory)
         {
-            Debug.LogFormat("{0} - Here!", partyMember);
+            Debug.LogFormat("Item: {0} - {1}g", kvp.Key, kvp.Value);
         }
     }
 }
