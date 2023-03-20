d0aaedc00d4ac0f00df90dda6241daf2526a92c5
Joshua Ryan-Sprowson <20025016@tafe.wa.edu.au>
2023-03-20 22:04:08 +0800

    Finish hero's trial - finding affordable items

--- Assets/Scripts/LearningCurve.cs
+++ Assets/Scripts/LearningCurve.cs
@@ -6,6 +6,8 @@ public class LearningCurve : MonoBehaviour
 {
     private void Start()
     {
+        int playerGold = 8;
+
         Dictionary<string, int> itemInventory = new Dictionary<string, int>()
         {
             { "Potion", 5 },
@@ -16,6 +18,11 @@ public class LearningCurve : MonoBehaviour
         foreach(KeyValuePair<string, int> kvp in itemInventory)
         {
             Debug.LogFormat("Item: {0} - {1}g", kvp.Key, kvp.Value);
+
+            if (playerGold >= kvp.Value)
+            {
+                Debug.LogFormat("This {0} item is affordable", kvp.Key);
+            }
         }
     }
 }
