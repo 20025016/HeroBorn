f5704834b0e32e74473cf39378519c8ae8cf494d
Joshua Ryan-Sprowson <20025016@tafe.wa.edu.au>
2023-03-24 22:58:03 +0800

    Finish rigidbody components in motion of working with the unity physics system section

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -10,13 +10,31 @@ public class PlayerBehaviour : MonoBehaviour
     private float _verticalInput;
     private float _horizontalInput;
 
+    private Rigidbody _rb;
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
     private void Update()
     {
         _verticalInput = Input.GetAxis("Vertical") * MoveSpeed;
         _horizontalInput = Input.GetAxis("Horizontal") * RotateSpeed;
 
-        this.transform.Translate(Vector3.forward * _verticalInput * Time.deltaTime);
+        //this.transform.Translate(Vector3.forward * _verticalInput * Time.deltaTime);
 
-        this.transform.Rotate(Vector3.up * _horizontalInput * Time.deltaTime);
+        //this.transform.Rotate(Vector3.up * _horizontalInput * Time.deltaTime);
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 rotation = Vector3.up * _horizontalInput;
+
+        Quaternion angleRotation = Quaternion.Euler(rotation * Time.fixedDeltaTime);
+
+        _rb.MovePosition(this.transform.position + this.transform.forward * _verticalInput * Time.fixedDeltaTime);
+
+        _rb.MoveRotation(_rb.rotation * angleRotation);
     }
 }
