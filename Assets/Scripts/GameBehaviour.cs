3a69b786020b2cb5f5161b67e1a57130586d050b
Joshua Ryan-Sprowson <20025016@tafe.wa.edu.au>
2023-03-25 11:36:53 +0800

    Finish the get and set properties of creating a game manager section

--- Assets/Scripts/GameBehaviour.cs
+++ Assets/Scripts/GameBehaviour.cs
@@ -6,4 +6,24 @@ public class GameBehaviour : MonoBehaviour
 {
     private int _itemsCollected = 0;
     private int _playerHealth = 10;
+
+    public int Items
+    {
+        get { return _itemsCollected; }
+        set
+        {
+            _itemsCollected = value;
+            Debug.LogFormat("Items: {0}", _itemsCollected);
+        }
+    }
+
+    public int PlayerHealth
+    {
+        get { return _playerHealth; }
+        set
+        {
+            _playerHealth = value;
+            Debug.LogFormat("Lives: {0}", _playerHealth);
+        }
+    }
 }
