75bc49cb6b8154332e7016271293b6353afaf6c4
Joshua Ryan-Sprowson <20025016@tafe.wa.edu.au>
2023-03-25 11:29:32 +0800

    Finish tracking player properties of creating a game manager section

---
+++ Assets/Scripts/GameBehaviour.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameBehaviour : MonoBehaviour
+{
+    private int _itemsCollected = 0;
+    private int _playerHealth = 10;
+}
